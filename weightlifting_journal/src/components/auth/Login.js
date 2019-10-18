63aa17f1fa948a0e5b9d4fcdbc44492c76ca92ce
Zach Taylor <zbtaylor1@gmail.com>
2019-10-17 22:02:13 -0400

    implemented the withFormik higher order component

--- weightlifting_journal/src/components/auth/Login.js
+++ weightlifting_journal/src/components/auth/Login.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Formik } from "formik";
+import { Form, Field, withFormik } from "formik";
 import Axios from "axios";
+import * as Yup from "yup";
 
-const Login = () => {
+const LoginForm = ({ errors, touched }) => {
   const handleSubmit = e => {
     e.preventDefault();
 
@@ -17,14 +18,55 @@ const Login = () => {
   };
 
   return (
-    <Formik>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="username" />
-        <input type="password" name="password" />
-        <button type="submit">Submit</button>
-      </form>
-    </Formik>
+    <Form onSubmit={handleSubmit}>
+      <fieldset>
+        <label>Username</label>
+        {touched.username && errors.username && (
+          <p className="error">{errors.username}</p>
+        )}
+        <Field type="text" name="username" placeholder="Arnold" />
+      </fieldset>
+      <fieldset>
+        <label>Password</label>
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
+        <Field type="text" name="password" placeholder="******" />
+      </fieldset>
+      <button type="submit">Submit</button>
+    </Form>
   );
 };
+
+const Login = withFormik({
+  // ES6 method definition shorthand
+  mapPropsToValues({ username, password }) {
+    return {
+      username: username || "",
+      password: password || ""
+    };
+  },
+
+  /*
+   * Old JS for the above would be:
+   * mapPropsToValues: function () {...},
+   */
+
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required("Username is required."),
+    password: Yup.string().required("Password is required.")
+  }),
+
+  handleSubmit(values, { props }) {
+    const dummyCredentials = {
+      username: "admin",
+      password: "password"
+    };
+
+    Axios.post("auth/login", dummyCredentials).then(res => {
+      console.log(res);
+    });
+  }
+})(LoginForm);
 
 export default Login;
