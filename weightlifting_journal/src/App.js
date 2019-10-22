2478801af1ee4ec89c053c9e9a21b2db0d4581e2
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-10-21 22:10:58 -0400

    Create CreateExercise.js

--- weightlifting_journal/src/App.js
+++ weightlifting_journal/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./interceptor";
 import { Route } from "react-router-dom";
 import Login from "./components/auth/Login";
+import CreateExercise from "./components/journals/CreateExercise.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Route path="/login" exact component={Login} />
+      <Route path="/exercise" component={CreateExercise} />
     </div>
   );
 }
